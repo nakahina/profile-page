429a0dbb93539fc9078d9344d309315f2f7e5f1a
nakahina <nakahina2014@gmail.com>
2025-04-26 15:42:46 +0900

    modify main page

---
+++ app/hobby/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Hobby = () => {
+  return (
+    <div>
+      <div>My Hobby</div>
+      <Link href="/">Homeへ</Link>
+    </div>
+  );
+};
+
+export default Hobby;
--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex items-center justify-center flex-col w-full h-screen">
+      <div className="rounded-lg p-10 bg-blue-500 shadow-lg shadow-blue-500/50 flex items-center justify-center flex-col">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          className="rounded-full mb-5"
+          src="/avator.png"
+          alt="プロフィール画像"
+          width={300}
+          height={300}
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          Hello World
+        <div className="text-white font-bold mb-5">中村 日向子</div>
+        <div className="text-white">
+          <div className="mb-5">
+            <div className="font-bold mb-3">自己紹介</div>
+            渋谷のベンチャー企業でWEBエンジニアしています👩🏻‍💻
+            <br />
+            主にTypeScript、React、Next.jsでの開発経験があります。
+          </div>
+          <div className="mb-7">
+            <div className="font-bold mb-3">趣味</div>
+            餃子製造
+          </div>
+          <Link href="/hobby">
+            <div className="mb-5 font-bold  bg-blue-800 hover:bg-blue-900 p-3 rounded-md text-center shadow-lg duration-300">
+              もっと見る
+            </div>
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
